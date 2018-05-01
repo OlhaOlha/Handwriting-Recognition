5b105c4efa946ee3320f1b9090c1d189e8764eef
olgamulava <olha.m@icloud.com>
2018-05-01 15:48:38 +0200

    Update font2image.py and otsu.py

--- otsu.py
+++ otsu.py
@@ -12,37 +12,37 @@ pathOut1 = "/Users/olgamulava/Documents/Uni/5Groningen/HandwritingRecognition/Co
 pathOut2 = "/Users/olgamulava/Documents/Uni/5Groningen/HandwritingRecognition/Code/OutputGAU/"
 pathOut3 = "/Users/olgamulava/Documents/Uni/5Groningen/HandwritingRecognition/Code/OutputMEAN/"
 
-
-img = cv2.imread(pathIn + "P344-Fg001-R-C01-R01-fused.jpg", 0)  # 0 - grayscale
-# cv2.imshow('My image', img)
-
-ret, imgBin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-# cv2.imwrite('pathOut1/otsuFirst.png', imgBin)
-
-# Original image
-plt.subplot(3, 1, 1)
-plt.imshow(img, cmap='gray')
-plt.title('Original Image')
-plt.xticks([])
-plt.yticks([])
-
-# Histogram
-plt.subplot(3, 1, 2)
-plt.hist(img.ravel(), 256)
-plt.axvline(x=ret, color='r', linestyle='dashed', linewidth=2)
-plt.title('Histogram')
-plt.xticks([])
-plt.yticks([])
-
-# Binarized image
-plt.subplot(3, 1, 3)
-plt.imshow(imgBin, cmap='gray')  # show binarized image
-plt.title('Otsu thresholding')
-plt.xticks([])
-plt.yticks([])
-plt.savefig(pathOut1 + "/otsuFirstGraph.png", dpi=500)
-plt.show()
+# # Create a graph for one image P344-Fg001-R-C01-R01-fused.jpg
+# img = cv2.imread(pathIn + "P344-Fg001-R-C01-R01-fused.jpg", 0)  # 0 - grayscale
+# # cv2.imshow('My image', img)
+#
+# ret, imgBin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+#
+# # cv2.imwrite('pathOut1/otsuFirst.png', imgBin)
+#
+# # Original image
+# plt.subplot(3, 1, 1)
+# plt.imshow(img, cmap='gray')
+# plt.title('Original Image')
+# plt.xticks([])
+# plt.yticks([])
+#
+# # Histogram
+# plt.subplot(3, 1, 2)
+# plt.hist(img.ravel(), 256)
+# plt.axvline(x=ret, color='r', linestyle='dashed', linewidth=2)
+# plt.title('Histogram')
+# plt.xticks([])
+# plt.yticks([])
+#
+# # Binarized image
+# plt.subplot(3, 1, 3)
+# plt.imshow(imgBin, cmap='gray')  # show binarized image
+# plt.title('Otsu thresholding')
+# plt.xticks([])
+# plt.yticks([])
+# plt.savefig(pathOut1 + "/otsuFirstGraph.png", dpi=500)
+# plt.show()
 
 
 # Open the folder with input images
