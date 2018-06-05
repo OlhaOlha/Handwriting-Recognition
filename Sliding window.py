db16b34c8e647c9a6556de1884dd25fdb903ce31
gajjarjanak <38045124+gajjarjanak@users.noreply.github.com>
2018-06-05 16:06:03 +0200

    Update Sliding window.py

--- Sliding window.py
+++ Sliding window.py
@@ -3,18 +3,14 @@ import cv2
 
 def sliding_window(image, stepSize, windowSize):
 	# slide a window across the image
-	for y in range(0, image.shape[0], stepSize):
-		for x in range(0, image.shape[1], stepSize):
-			# yield the current window
-			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+		# yield the current window
+	        yield (image.shape[1]-x, y, image[y:y + windowSize[1], image.shape[1]-x:image.shape[1]-x + windowSize[0]])
 
 #clf = joblib.load("digits_cls.pkl")    #mnist trained classifier
-img = cv2.imread('C:\\Users\\Janak\\Desktop\\Handwriting Recognition\\Input\\d1cde2cb-75bc-432f-a7eb-521a99824939.jpg', 0)
-winW, winH = (22, 40)
-
-blur = cv2.GaussianBlur(img, (5,5),0)
-thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)
+img = cv2.imread('C:\\Users\\Janak\\Desktop\\Handwriting Recognition\\Input\\075bf865-70bb-43a9-9b51-619fb0275d04.jpg', 0)
+winW, winH = (int(img.shape[0]/2), img.shape[0])
 
 for (x, y, window) in sliding_window(img, stepSize=10, windowSize=(winW, winH)):
     if (window.shape[0] != winH or window.shape[1] != winW):
